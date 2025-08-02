d56ffaa4bf8ab6c0240d6963237c2078c7467448
Arthur <lututuibr@gmail.com>
2025-08-02 20:23:27 -0300

    Force window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:watermelon_game/game.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
-  runApp(
-    GameWidget.controlled(
-      gameFactory: WatermelonGame.new,
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+  await windowManager.setSize(Size(450, 600));
+  await windowManager.setResizable(false);
+  await windowManager.setMaximizable(false);
+  await windowManager.show();
+
+  runApp(GameWidget.controlled(gameFactory: WatermelonGame.new));
 }
